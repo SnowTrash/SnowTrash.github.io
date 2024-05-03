8df58d0d384013b0a2849216733a1642c80352f8
SnowTrash <juan.vargas2962@alumnos.udg.mx>
2024-05-03 10:41:07 -0600

    3d
    añadiendo TresJs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 export default defineNuxtConfig({
   app: {
-    baseURL: '/nuxt-github-pages/',
+    baseURL: '/',
     buildAssetsDir: 'assets'
-  }
+  },
+  tres: {
+    devtools: true,
+    glsl: true
+  },
+  modules: ['@tresjs/nuxt', '@nuxt/devtools' ]
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,38 @@
+<script lang="ts" setup>
+import { BlendFunction } from 'postprocessing';
+const sphereRef = ref()
+
+const { onLoop } = useRenderLoop()
+
+onLoop(({ elapsed }) => {
+  if (sphereRef.value) {
+    sphereRef.value.position.y = Math.sin(elapsed)
+  }
+})
+
+const bloomParams = reactive({
+  luminanceThreshold: 0.2,
+  luminanceSmoothing: 0.3,
+  mipmapBlur: true,
+  intensity: 0.3,
+  blendFunction: BlendFunction.ADD,
+})
+</script>
+
 <template>
-  <main>
-    <div>
-      <h1>Home</h1>
-      <NuxtLink to="/about">About</NuxtLink>
-    </div>
-    <img src="/qingbao-meng-01_igFr7hd4-unsplash.jpg" loading="lazy">
-  </main>
+  <TresCanvas window-size clear-color="#4f4f4f">
+    <TresPerspectiveCamera :position="[-5.3, 8.3, 10.6]" :look-at="[0, 0, 0]" />
+    <OrbitControls />
+    <TresMesh>
+      <TresSphereGeometry />
+      <TresMeshNormalMaterial />
+    </TresMesh>
+    <TresGridHelper />
+    <!-- <Suspense>
+      <Stones />
+    </Suspense>
+    <EffectComposer>
+      <Bloom v-bind="bloomParams" />
+    </EffectComposer> -->
+  </TresCanvas>
 </template>
